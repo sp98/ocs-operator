8cb512a02eca0da7f9ac0e807e976d65f170e22d
kesavan <kvellalo@redhat.com>
2022-02-21 15:56:11 +0530

    Controller: StorageConsumer fix kind in cephResources status
    Signed-off-by: kesavan <kvellalo@redhat.com>
    (cherry picked from commit 35cc79e3aa2d3e3476a3961575ffe766162240af)
    Signed-off-by: Santosh Pillai <sapillai@redhat.com>

--- services/provider/server/server.go
+++ services/provider/server/server.go
@@ -224,6 +224,11 @@ func newClient() (client.Client, error) {
 		return nil, fmt.Errorf("failed to add ocsv1alpha1 to scheme. %v", err)
 	}
 
+	err = rookCephv1.AddToScheme(scheme)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add rookCephv1 to scheme. %v", err)
+	}
+
 	config, err := config.GetConfig()
 	if err != nil {
 		klog.Error(err, "failed to get rest.config")
